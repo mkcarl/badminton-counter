3d6176e5b6b215576ff3a306d0fd26e370381354
munky <munkye29@gmail.com>
2022-09-22 15:34:17 +0800

    update 1

---
+++ src/components/Scoreboard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Typography} from "@mui/material";
+
+export class Scoreboard extends React.Component{
+    render() {
+        return (
+            <Typography variant={"h1"}>Scoreboard</Typography>
+        );
+    }
+}
